befe78e4062a8908e7fabb396842aabc94f73993
Giorgix <jackosinapsis@gmail.com>
2015-08-01 14:07:04 -0400

    changed overlay styles

--- public/js/ux.js
+++ public/js/ux.js
@@ -9,9 +9,44 @@ for(var i=0;i<close.length;i++){
 }
 
 function overlay(e) {
+  this.className == "close" ? enableScroll() : disableScroll();
   e.preventDefault();
   var attribute = this.getAttribute("data-info");
   el = document.getElementById("overlay" + attribute);
-  console.log(attribute);
   el.style.visibility = (el.style.visibility == "visible") ? "hidden" : "visible";
+  el.style.visibility == "visible" ? el.className = "overlay animated fadeIn" : el.className = "overlay";
+}
+
+var keys = {37: 1, 38: 1, 39: 1, 40: 1};
+
+function preventDefault(e) {
+  e = e || window.event;
+  if (e.preventDefault)
+      e.preventDefault();
+  e.returnValue = false;  
+}
+
+function preventDefaultForScrollKeys(e) {
+    if (keys[e.keyCode]) {
+        preventDefault(e);
+        return false;
+    }
+}
+
+function disableScroll() {
+  if (window.addEventListener) // older FF
+      window.addEventListener('DOMMouseScroll', preventDefault, false);
+  window.onwheel = preventDefault; // modern standard
+  window.onmousewheel = document.onmousewheel = preventDefault; // older browsers, IE
+  window.ontouchmove  = preventDefault; // mobile
+  document.onkeydown  = preventDefaultForScrollKeys;
+}
+
+function enableScroll() {
+    if (window.removeEventListener)
+        window.removeEventListener('DOMMouseScroll', preventDefault, false);
+    window.onmousewheel = document.onmousewheel = null; 
+    window.onwheel = null; 
+    window.ontouchmove = null;  
+    document.onkeydown = null;  
 }
