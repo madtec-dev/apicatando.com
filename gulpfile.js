6d9e2da3d2bc03f5f7a519488c0915f227fc1517
Giorgix <jackosinapsis@gmail.com>
2015-08-04 21:54:36 -0400

    added production gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,12 @@
 var gulp = require('gulp'),
+  concat = require('gulp-concat'),
   nodemon = require('gulp-nodemon'),
   plumber = require('gulp-plumber'),
+  uglify = require('gulp-uglify'),
+  jshint = require('gulp-jshint'),
   livereload = require('gulp-livereload'),
-  sass = require('gulp-ruby-sass');
+  sass = require('gulp-ruby-sass'),
+  minifyCss = require('gulp-minify-css');
 
 gulp.task('sass', function () {
   return sass('./public/css/')
@@ -26,8 +30,29 @@ gulp.task('develop', function () {
   });
 });
 
+gulp.task('minify-css', function() {
+    return gulp.src('./public/css/*.css')
+        .pipe(minifyCss({compatibility: 'ie8'}))
+        .pipe(concat('style.min.css'))
+        .pipe(gulp.dest('./public/dst'));
+});
+
+gulp.task('js', function() {
+  return gulp.src(['./public/js/responsive-nav.js', './public/js/scroll.js', './public/js/fixed-responsive-nav.js', './public/js/ux.js'])
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'))
+    .pipe(uglify())
+    .pipe(concat('app.min.js'))
+    .pipe(gulp.dest('./public/dst'));
+});
+
 gulp.task('default', [
   'sass',
   'develop',
   'watch'
+]);
+
+gulp.task('production', [
+  'minify-css',
+  'js'
 ]);
